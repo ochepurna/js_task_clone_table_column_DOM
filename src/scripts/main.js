ba7229b9667149f25aaa9dee45ca0fc80e7573ad
Olena Chepurna <lchep@ukr.net>
2025-05-10 00:13:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
 // write your code here
+document.querySelectorAll('table tr').forEach((row) => {
+  const secondCell = row.cells[1];
+  const copy = secondCell.cloneNode(true);
+  const cells = row.cells;
+
+  row.insertBefore(copy, cells[cells.length - 1]);
+});
